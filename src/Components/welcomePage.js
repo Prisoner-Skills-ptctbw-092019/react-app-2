e6de8f96ccc0733776998166567f920c300e9347
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2019-09-24 17:52:18 -0600

    added-router/component-functionality

---
+++ src/Components/welcomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card } from "semantic-ui-react";
+
+export default function WelcomePage(props) {
+  return (
+    <Card className="Welcome-page">
+      <h1>Welcome</h1>
+    </Card>
+  );
+}
